61299ba8b95dab95e9c2fcb684396101d98c7348
mkpbd <mostofakamal1990@gmail.com>
2022-01-27 18:31:50 +0600

    udate content

--- js/main.js
+++ js/main.js
@@ -414,3 +414,157 @@ $(function () {
 			
 		}
 	}
+
+	document.getElementById('find_doctor_id').addEventListener('click',function(){
+		let dblock = document.querySelectorAll('.doctor-text-indent');
+		let mm = Array.from(dblock);
+			mm[0].classList.toggle("toggle-txt-indent");
+			mm[1].classList.toggle("toggle-txt-indent");
+		document.querySelector('section.find-doctor').classList.toggle('toggle-find-doctor');
+		document.getElementById('find_doctor_id').classList.toggle("doctor-button-modify");
+	});
+
+	document.getElementById('close_id').addEventListener('click',function(){
+		let dblock = document.querySelectorAll('.doctor-text-indent');
+		let mm = Array.from(dblock);
+		mm[0].classList.toggle("toggle-txt-indent");
+		mm[1].classList.toggle("toggle-txt-indent");
+		document.querySelector('section.find-doctor').classList.toggle('toggle-find-doctor');
+		document.getElementById('find_doctor_id').classList.toggle("doctor-button-modify");
+	});
+
+	document.getElementById('icon_open').addEventListener('click',function(){
+		let dblock = document.querySelectorAll('.doctor-text-indent');
+		let mm = Array.from(dblock);
+			mm[0].classList.toggle("toggle-txt-indent");
+			mm[1].classList.toggle("toggle-txt-indent");
+		document.querySelector('section.find-doctor').classList.toggle('toggle-find-doctor');
+		document.getElementById('find_doctor_id').classList.toggle("doctor-button-modify");
+	});
+
+
+
+	
+	// Appointment By Male 
+
+	function sendEmail() {
+		let department = document.getElementById("department");
+		let firstName = document.getElementById("firstName");
+		let lastName = document.getElementById("lastName");
+		let phone = document.getElementById("phone");
+		let appointmentDate = document.getElementById("appointmentDate");
+		let appointmentTime = document.getElementById("appointmentTime");
+		let message = document.getElementById('message');
+		let appointmentMessage =document.getElementsByClassName('message-appointments');
+		
+		let html = `
+			<html>
+				<head>
+				</head>
+				<body>
+					<h4> Application Date:${new Date().toUTCString()} </h4>
+					<h3> Full Name : ${firstName.value} ${lastName.value} </h4>
+					<h3> Mobile No : ${phone.value} </h3>
+					<h4>Department: ${department.value}</h4>
+					<h4>Application Date and Time: ${appointmentDate.value} ${appointmentTime.value}</h4>
+					<h4>Message : ${message.value}</h4>
+				
+				<body>
+			</html>
+		`;
+
+		Email.send({
+		  //Host: "smtp.gmail.com",
+		  SecureToken:"a0e99564-6ccc-4104-9293-5b29f2ebe8cf",
+		  //Username: "emsl.mostofakamal@gmail.com",
+		  //Password: "mk6683mk*",
+		  To: 'mostofa1990mkp@gmail.com',
+		  // From: "mostofakamal1990@gmail.com",
+		  From: "01720246683@gmail.com",
+		  Subject: "Appointments",
+		  Body: html,
+		  //a0e99564-6ccc-4104-9293-5b29f2ebe8cf 
+		 
+		})
+		  .then(function (message) {
+			console.log(message);
+			  //alert("Mail has been sent successfully")
+			  appointmentMessage[0].style.display='block';
+			  successMessage();
+		  }).catch(function(error){
+			appointmentMessage[1].style.display='block';
+			successMessage();
+		  });
+	  }
+
+
+	  function successMessage(){
+		let appointmentMessage =document.getElementsByClassName('message-appointments');
+
+		setTimeout(function(){
+			appointmentMessage[0].style.display='none';
+		},4000);
+	  }
+
+
+
+
+	  // Click button Type For sending Mails
+	  document.getElementById('submitAppointment').addEventListener('click', function(){
+		let department = document.getElementById("department").value;
+		let firstName = document.getElementById("firstName");
+		let lastName = document.getElementById("lastName");
+		let phone = document.getElementById("phone");
+		let appointmentDate = document.getElementById("appointmentDate");
+		let appointmentTime = document.getElementById("appointmentTime");
+		let message = document.getElementById('message');
+		let errorM = document.getElementsByClassName('errorM');
+
+		//console.log( new Date().getDay());
+		if(firstName.value.trim().length <3){
+			errorM[0].style.border ="1px solid red";
+			console.log("hello")
+			return false;
+		}
+		errorM[0].style.border ="1px solid blue";
+
+		if(lastName.value.trim().length <3){
+			errorM[1].style.border ="1px solid red";
+			return false;
+		}
+		errorM[1].style.border ="1px solid blue";
+
+		if(phone.value.trim().length >=11 && phone.value.trim().length <=14){
+			errorM[3].style.border ="1px solid blue";
+			
+		}
+		else{
+			errorM[3].style.border ="1px solid red";
+			return false;
+		}
+
+
+		if(appointmentDate.value.trim().length >=8 && lastName.value.trim().length <=9){
+			errorM[4].style.border ="1px solid blue";
+			
+		} 
+		else{
+			errorM[4].style.border ="1px solid red";
+		}
+
+		if(message.value.trim().length < 10){
+			errorM[6].style.border ="1px solid red";
+			return false;
+		}
+		errorM[6].style.border ="1px solid blue";
+		
+		
+		sendEmail();
+
+	  });
+
+	  
+
+	//   document.getElementById('readmore').addEventListener("click", function(){
+	// 	  alert("ok this button work properly");
+	//   })
